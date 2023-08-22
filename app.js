ef4e8a0ce2901ce6c5d9eb839552297e5a2fbf61
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-08-22 09:23:42 +0400

    feat: Создать схемы и модели проекта

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
 
 app.use(requestLogger);
 
