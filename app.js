41c54d3457801e60c2164cc0ddff697ac15a729f
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-09-18 21:47:19 +0400

    feat: Добавить CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 require('dotenv').config();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
@@ -16,6 +17,11 @@ app.use(cookieParser());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: ['https://movies--explorer.nomoredomainsicu.ru/signup'],
+  credentials: true,
+}));
 
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
